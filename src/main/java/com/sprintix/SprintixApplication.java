a9da9f5bc00806227278956edd80a18778c8b73e
Javieroliver-web <javip200555@gmail.com>
2025-11-26 14:02:16 +0100

    Separacion de corsconfig a archivo propio

--- src/main/java/com/sprintix/SprintixApplication.java
+++ src/main/java/com/sprintix/SprintixApplication.java
@@ -2,9 +2,6 @@ package com.sprintix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SprintixApplication {
@@ -12,18 +9,5 @@ public class SprintixApplication {
     public static void main(String[] args) {
         SpringApplication.run(SprintixApplication.class, args);
         System.out.println("🚀 API Spring Boot corriendo en http://localhost:8080/api");
-    }
-
-    // Configuración CORS Global (Para que Angular pueda entrar)
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") // Permitir acceso a todos (Frontend)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
     }
 }
---
+++ src/main/java/com/sprintix/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.sprintix.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas de la API
+                        .allowedOrigins("http://localhost:4000", "http://localhost:8081") // Permitir a tu Web
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
